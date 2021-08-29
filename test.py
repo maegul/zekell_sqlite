e200dd7d682634fbd493453cac5271fb02ab8c46
Errol Lloyd <errolkjlloyd@gmail.com>
2021-08-29 17:44:22 +1000

    test tag adding function

--- test.py
+++ test.py
@@ -1,5 +1,7 @@
 from pathlib import Path
 import unittest
+import sqlite3 as sql
+from unittest.main import TestProgram
 
 import zekell
 
@@ -27,10 +29,38 @@ class TestCreateTables(unittest.TestCase):
 
         self.assertEqual(len(table_check), 1)
 
+    def test_root_tag_unique(self):
+        db = self._db
+
+        test_tag = 'first_root'
+        test_parent_id = None
+
+        zekell.add_tag(db, test_tag, parent_id=test_parent_id)
+
+        with self.assertRaises(sql.IntegrityError):
+            zekell.add_tag(db, test_tag, parent_id=test_parent_id)
+
+    def test_tag_unique(self):
+        db = self._db
+
+        test_tag = 'test_tag'
+        test_parent_id = db.ex('select * from tags')[0][0]
+
+        zekell.add_tag(db, test_tag, test_parent_id)
+
+        with self.assertRaises(sql.IntegrityError):
+            zekell.add_tag(db, test_tag, test_parent_id)
+
+    def test_parent_id_foreign_key(self):
+
+        with self.assertRaises(sql.IntegrityError):
+            zekell.add_tag(self._db, 'bad_parent_tag', 111)
+
     @classmethod
     def tearDownClass(cls) -> None:
         cls._db.conn.close()
         cls._db_path.unlink()
+
 
 
 if __name__ == '__main__':
