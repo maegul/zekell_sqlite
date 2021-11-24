154b08b67235d2147c377122bf0b0ca7da05ee5f
Errol Lloyd <errolkjlloyd@gmail.com>
2021-11-24 17:33:43 +1100

    CLI command to generate and display graph using graphviz

--- zekell.py
+++ zekell.py
@@ -1243,6 +1243,44 @@ def cli_search(args):
 
     display_rows(db, result)
 
+def cli_graph(_):
+
+    db = db_connection(ZK_DB_PATH)
+    q = '''
+        select a.parent_note_id, b.title, a.child_note_id, c.title
+        from note_links a
+        left join notes b on a.parent_note_id = b.id
+        left join notes c on a.child_note_id = c.id
+    '''
+
+    links = db.ex(q)
+
+    all_nodes = set()
+    for l in links:
+        all_nodes.add((l[0], l[1]))
+        all_nodes.add((l[2], l[3]))
+
+    lines = []
+    lines.append('digraph test {\n')
+    lines.append('rankdir=LR;\n')
+    for n in all_nodes:
+        lines.append(f'\t{n[0]} [label="{n[1]}"]\n')
+    lines.append('\n')
+    for l in links:
+        lines.append(f'\t{l[0]} ->{l[2]}\n')
+    lines.append('}')
+
+    graph_file = ZK_PATH / Path('zk_graph.dot')
+    out_file = graph_file.with_suffix('.svg')
+
+    with open(graph_file, 'w') as f:
+        f.writelines(lines)
+
+    _ = sp.check_output(
+            ['dot', '-Tsvg', graph_file.as_posix(), '-o', out_file.as_posix()]
+        )
+
+    _ = sp.check_output(['open', '-a', 'Safari', out_file.as_posix()])
 
 def main():
     parser = ArgumentParser(
@@ -1315,7 +1353,7 @@ def main():
         help='Run a sql select query against the database')
     ap_query.add_argument('query',
         help='Query to run, will be automatically preceded by "select "')
-    ap_query.add_argument('--limit',
+    ap_query.add_argument('-l', '--limit',
         help='Number to limit return by (default 100)',
         default = 100)
 
@@ -1328,6 +1366,10 @@ def main():
         Query to search with.
         Format: "keyword: query; ...".
         Keywords: {', '.join(cte_map.keys())}''')
+
+    ap_graph = subparsers.add_parser('graph', description='Make graph of zetellkasten',
+        help='Use graphviz (dot) to make svg graph and open with Safar'
+        )
 
 
 
@@ -1357,6 +1399,8 @@ def main():
         cli_query(args)
     elif args.sub_command == 'q':
         cli_search(args)
+    elif args.sub_command == 'graph':
+        cli_graph(args)
 
 if __name__ == '__main__':
     main()
